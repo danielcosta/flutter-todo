4cd4967a5f647b50e966086f22bc699c6409fffc
Daniel Costa <danielcosta@gmail.com>
2019-10-09 22:26:17 -0300

    final

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
-
+import 'package:shared_preferences/shared_preferences.dart';
 import 'models/item.dart';
 
 void main() => runApp(MyApp());
@@ -23,9 +24,9 @@ class HomePage extends StatefulWidget {
 
   HomePage() {
     items = [];
-    items.add(Item(title: 'Item 1', done: false));
-    items.add(Item(title: 'Item 2', done: true));
-    items.add(Item(title: 'Item 3', done: false));
+    // items.add(Item(title: 'Item 1', done: false));
+    // items.add(Item(title: 'Item 2', done: true));
+    // items.add(Item(title: 'Item 3', done: false));
   }
   @override
   _HomePageState createState() => _HomePageState();
@@ -45,7 +46,37 @@ class _HomePageState extends State<HomePage> {
         ),
       );
       newTaskCtrl.text = '';
+      save();
     });
+  }
+
+  void remove(int index) {
+    setState(() {
+      widget.items.removeAt(index);
+      save();
+    });
+  }
+
+  Future load() async {
+    var prefs = await SharedPreferences.getInstance();
+    var data = prefs.getString('data');
+
+    if (data != null) {
+      Iterable decoded = jsonDecode(data);
+      List<Item> result = decoded.map((x) => Item.fromJson(x)).toList();
+      setState(() {
+        widget.items = result;
+      });
+    }
+  }
+
+  save() async {
+    var prefs = await SharedPreferences.getInstance();
+    await prefs.setString('data', jsonEncode(widget.items));
+  }
+
+  _HomePageState() {
+    load();
   }
 
   @override
@@ -78,6 +109,7 @@ class _HomePageState extends State<HomePage> {
               onChanged: (value) {
                 setState(() {
                   item.done = value;
+                  save();
                 });
               },
             ),
@@ -85,6 +117,9 @@ class _HomePageState extends State<HomePage> {
             background: Container(
               color: Colors.red.withOpacity(0.2),
             ),
+            onDismissed: (direction) {
+              remove(index);
+            },
           );
         },
       ),
